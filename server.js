6e5c5f141e83c435210a02bebd0ed7eeafb410a3
mtiosejo <mtiosejo@gmail.com>
2018-11-27 20:45:56 -0800

    Configured server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const app = express();
+const port = 8081;
+
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/:propertyID', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/public/index.html`));
+});
+
+app.listen(port, () => {
+  console.log(`server running at http://localhost:${port}`);
+});
